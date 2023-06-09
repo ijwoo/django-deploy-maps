e5b15a1cf14c5f80a7bf071f43ffb4719c820066
Jaewoo Im <94038275+ijwoo@users.noreply.github.com>
2023-06-09 15:50:52 +0900

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-*y8s-o#xne$h294h1w1n@u^jfm#-8hd89p3_v4bb=katwbvq^#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    ".ap-northeast-2.compute.amazonaws.com"
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
